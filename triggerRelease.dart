774699775b5b4f8554bb18286d3f972953811f05
Robert Sutton <rlsutton152@gmail.com>
2023-01-06 14:34:02 +1100

    for version 3.35.0-nj

--- triggerRelease.dart
+++ triggerRelease.dart
@@ -15,6 +15,7 @@ void main() {
   read(join(dir, "pom.xml")).forEach((line) {
     if (line.contains("<releaseVersion>")) {
       line = line.replaceFirst("-SNAPSHOT", "");
+      line = line.replaceFirst("-nj", "");
       var parts = line.split(".");
       if (parts.length != 3) {
         exit(1);
@@ -25,14 +26,9 @@ void main() {
     }
   });
 
-  var postFix = "";
-  if (confirm("Is this a SNAPSHOT release (y/n)?")) {
-    postFix = "-SNAPSHOT";
-    rev++;
-  } else {
-    rev = 0;
-    minor++;
-  }
+  var postFix = "-nj";
+  rev = 0;
+  minor++;
 
   String version = "$major.$minor.$rev$postFix";
 
